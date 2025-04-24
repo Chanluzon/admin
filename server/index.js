6c321b5c117b486bb23d3994e197080ebd309969
Christ Amron A. Luzon <11ict1.luzon@gmail.com>
2025-04-24 10:12:04 +0800

    Update server to use environment variables for Firebase config

--- server/index.js
+++ server/index.js
@@ -2,9 +2,22 @@ const express = require('express');
 const cors = require('cors');
 const admin = require('firebase-admin');
 const adminRoutes = require('./routes/admin');
+require('dotenv').config();
 
 // Initialize Firebase Admin
-const serviceAccount = require('./config/serviceAccountKey.json');
+const serviceAccount = {
+  "type": "service_account",
+  "project_id": "appdev-86a96",
+  "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+  "private_key": process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+  "client_id": process.env.FIREBASE_CLIENT_ID,
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": process.env.FIREBASE_CLIENT_CERT_URL
+};
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://appdev-86a96-default-rtdb.asia-southeast1.firebasedatabase.app"
@@ -14,7 +27,11 @@ const app = express();
 
 // Configure CORS
 app.use(cors({
-  origin: /^http:\/\/localhost:\d+$/, // Allow any localhost port
+  origin: [
+    /^http:\/\/localhost:\d+$/, // Allow any localhost port
+    'https://grabi-admin.netlify.app', // Allow Netlify domain
+    'https://*.netlify.app' // Allow all Netlify subdomains
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
